a6cb22724ab038be6041c060704e58c80565eac7
jamesyeomans <jamezyeomans@gmail.com>
2017-12-07 19:35:08 +0000

    Paraxify and Slick integrated and initialised

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -7,8 +7,13 @@ import Foundation from 'foundation-sites';
 // If you want to pick and choose which modules to include, comment out the above and uncomment
 // the line below
 //import './lib/foundation-explicit-pieces';
+
+import './lib/paraxify';
+import './lib/slick';
+
 $(document).foundation();
 
+//Top Bar Scroll Event
 $(window).scroll(function(e){
     e.preventDefault();
     if(!$(window).scrollTop()) {
@@ -16,4 +21,24 @@ $(window).scroll(function(e){
     }else{
         $('header.fixed_header').addClass('scrolled');
     }
+});
+
+// Paraxify Init
+var myParaxify = paraxify('.paraxify');
+
+// Slick Init
+
+var homepageSlider = $('.homepage_slider_wrapper');
+
+homepageSlider.slick({
+	dots: true,
+	arrows: false,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	infinite: true,
+	speed: 500,
+	fade: true,
+	autoplay: true,
+	autoplaySpeed: 5000,
+	pauseOnHover: false
 });
